a2b75a4b5709c7cf6cb7ad3298e1ba47fb78c690
Ralph Bourgoo <ralph.bourgoo@student.howest.be>
2021-03-29 12:14:19 +0200

    Link in oranje

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+export async function getStaticProps() {
+  const res = await fetch('https://localhost:1337/articles')
+  const posts = await res.json()
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
 export default function Home() {
   return (
     <div className={styles.container}>
